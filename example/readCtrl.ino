a982d07e74582a55129ad4a0fe4c4873ef263fb8
dcback <45322614+dcback@users.noreply.github.com>
2019-03-02 17:12:53 +0900

    Create readCtrl.ino

---
+++ example/readCtrl.ino
@@ -0,0 +1,16 @@
+#include "readCtrl.h"
+
+readCtrl Read_Control;
+
+extern float readValue;
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    Read_Control.readPin(A0);
+    Read_Control.writePin(13, 2.5);
+    Serial.print("read Value : ");
+    Serial.println(readValue);
+}
