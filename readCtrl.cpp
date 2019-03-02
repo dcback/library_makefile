0465dbb2404bc124242d498d957cfb68eb5d069f
dcback <45322614+dcback@users.noreply.github.com>
2019-03-02 17:14:42 +0900

    Create readCtrl.cpp

---
+++ readCtrl.cpp
@@ -0,0 +1,19 @@
+#include "readCtrl.h"
+#include "Arduino.h"
+
+float readValue;
+
+float readCtrl::readPin(int _aPin)
+{
+    readValue = analogRead(_aPin) * (5.0 / 1023.0);
+    return  readValue;
+}
+
+void readCtrl::writePin(int _dPin, float _refValue)
+{
+    pinMode( _dPin, OUTPUT );
+    if ( readValue > _refValue )
+        digitalWrite( _dPin, HIGH );
+    else
+        digitalWrite( _dPin, LOW );
+}
